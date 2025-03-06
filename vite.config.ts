526beca86326e109b56b62282547d97cc47205f9
Sanidhya's Office <apple@Apples-MacBook-Air.local>
2025-03-07 02:55:21 +0530

    Added Package

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import sitemap from 'vite-plugin-sitemap'
+
+// Define your site URL
+const SITE_URL = 'https://worthyjobs.in'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({
+      hostname: SITE_URL, // Your website URL
+      exclude: ['/404'], // Exclude pages like 404 if necessary
+    }),
+  ],
   base: "./",
   assetsInclude: ['**/*.ttf', '**/*.woff', '**/*.woff2', '**/*.eot'],
   build: {
     rollupOptions: {
       output: {
         assetFileNames: (assetInfo: { name?: string }) => {
-          const fontFiles = ['Nohemi-Regular.ttf', 'Nohemi-Bold.ttf', 'Nohemi-Medium.ttf', 'Nohemi-Light.ttf', 'Nohemi-SemiBold.ttf'];
+          const fontFiles = ['Nohemi-Regular.ttf', 'Nohemi-Bold.ttf', 'Nohemi-Medium.ttf', 'Nohemi-Light.ttf', 'Nohemi-SemiBold.ttf']
           if (assetInfo.name && fontFiles.some(font => assetInfo.name?.includes(font))) {
-            return 'fonts/[name][extname]';
+            return 'fonts/[name][extname]'
           }
-          return 'assets/[name]-[hash][extname]';
+          return 'assets/[name]-[hash][extname]'
         }
       }
     }
